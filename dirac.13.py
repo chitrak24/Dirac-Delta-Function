1b48c35e81b409a48e078e5b07ed1dc8c740b282
Chitrak Roychowdhury <99852681+chitrak24@users.noreply.github.com>
2022-03-08 22:00:33 +0530

    Add files via upload

---
+++ dirac.13.py
@@ -0,0 +1,63 @@
+"""
+Registration : 012-1111-0461-20
+Roll         : 203012-21-0008 
+Description  : Dirac Delta Functions
+Author       : Chitrak Roychowdhury
+"""
+
+
+#Dirac Delta Function
+#Representation using Airy Function
+#\epsilon ---> 0
+#func = airy(x/\epsilon)/\epsilon
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import airy
+from scipy.integrate import simps
+
+a = 1.0                #peak position of delta function
+x1 = a-5; x2 = a+5     #x-range for plotting peak of delta func
+
+#Representation of Dirac Delta function 
+def ddf(x,eps):
+    ai, aip, bi, bip = airy(x/eps)
+    return ai/eps
+print("Representation of Dirac Delta function using Airy Function")
+
+#Plot
+eps = 0.1
+x = np.linspace(x1, x2, 100)
+s = np.arange(1,5)
+for i in s:
+    eps = eps/i
+    plt.plot(x,ddf(x-a,eps),label="$\epsilon=%f$"%(eps))
+
+plt.xlabel("$x \longrightarrow$")
+plt.ylabel ("$\delta(x-a)$")
+plt.title("Delta Function")
+plt.legend(loc="best")
+plt.grid(True)
+plt.xlim(-5,7)
+plt.ylim(-60,50)
+plt.xticks()
+plt.yticks()
+plt.axvline(0, c='gray',ls='--', lw=1)
+plt.axhline(0, c='gray',ls=':', lw=1)
+plt.show()
+
+#verification of intrgration of del(x-a)g(x) for limit -inf to +inf = g(a)
+
+#function given
+def g(x):
+    return x**2
+
+#evaluating intrgral
+xp = np.linspace(a-5.1*eps,a+5.1*eps,10)
+I = simps(g(xp)*ddf(xp-a,eps),xp)
+
+#Exact result
+exact = g(a)
+
+#Print
+print("Calculated Result",I, "Exact Value",exact)
